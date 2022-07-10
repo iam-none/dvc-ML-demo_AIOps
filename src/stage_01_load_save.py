0c2e4f84571ae7cedd3ce533879ed64f13ed30fa
Ashirbad Das <ashirbad.mechanical@gmail.com>
2022-07-11 00:42:48 +0530

    stage 01 created

---
+++ src/stage_01_load_save.py
@@ -0,0 +1,33 @@
+# from webbrowser import get
+from src.utils.all_utils import read_yaml,create_dir
+import argparse
+import pandas as pd
+import os
+
+def get_data(config_path):
+    config=read_yaml(config_path)
+    
+    remote=config['data_source']
+    df=pd.read_csv(remote,sep=';')
+    # save dataset in the local
+    # create path to directory: artifacts/raw_local_dir/data.csv
+    artifacts_dir=config['artifacts']['artifacts_dir']
+    raw_local_dir=config['artifacts']['raw_local_dir']
+    raw_local_file=config['artifacts']['raw_local_file']
+
+    raw_local_dir_path=os.path.join(artifacts_dir,raw_local_dir)
+    create_dir(dirs=[raw_local_dir_path])
+    
+    raw_local_file_path=os.path.join(raw_local_dir_path,raw_local_file)
+    df.to_csv(raw_local_file_path,sep=',',index=False)
+
+
+    
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+
+    args.add_argument("--config", "-c", default="config/config.yaml")
+
+    parsed_args = args.parse_args()
+
+    get_data(config_path=parsed_args.config)
